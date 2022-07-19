0dcd414b5900f1bfb7b46acf01ae2b1775298c9c
Emad0007 <BASEMRF007@GMAIL.COM>
2022-07-19 13:43:49 +0100

    exo 2 try catch

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
 
-const getView = () => {
+// const getView = () => {
    
-    return new Promise((resolve, reject) => {
+//     return new Promise((resolve, reject) => {
       
-        setTimeout(() => {
-        console.log("Hello world")
-       }, 2000)
-     })
-}
+//         setTimeout(() => {
+//         console.log("Hello world")
+//        }, 2000)
+//      })
+// }
   
-const waitForView = async () => {
-const result = await getView()
-}
+// const waitForView = async () => {
+// const result = await getView()
+// }
   
- waitForView()
+//  waitForView()
 
  
+
+
+const getTemperature = () => {
+     return new Promise((resolve, reject) => {
+      setTimeout(() => {
+      
+        const Temperature  = 15
+        
+        if (Temperature <= 10) {
+          resolve("Il fait froid!")
+      
+        } else {
+          reject("Il fait chaud !")
+        }
+      }, 4000)
+     
+    })
+  }
+  
+    const waitForTemperature = async () => {
+   
+    try {
+          const result = await getTemperature() 
+          console.log(result)
+    } catch(error) {
+          console.log("la promesse est rejetée")
+        console.log(error)
+      }
+   
+  } 
+  
+  waitForTemperature()
